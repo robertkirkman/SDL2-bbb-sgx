7770d8e75049812a5de60fac70afa10e589ab020
Robert Kirkman <31490854+robertkirkman@users.noreply.github.com>
2021-07-18 20:46:21 -0500

    Update install-sgx.sh
    Add libibus-1.0-dev to dependencies for SDL2 input

--- install-sgx.sh
+++ install-sgx.sh
@@ -5,7 +5,7 @@ then
     echo 'APT::Default-Release "buster";' > /etc/apt/apt.conf.d/10default.conf
     apt-get update
     apt-get upgrade -y
-    apt-get install -y linux-image-4.14.108-ti-r131 linux-headers-4.14.108-ti-r131 ti-sgx-ti335x-modules-4.14.108-ti-r131 libdrm-dev ti-libgbm2-dev build-essential make cmake autoconf automake libtool libpulse-dev libdbus-1-dev libudev-dev libxkbcommon-dev python3 
+    apt-get install -y linux-image-4.14.108-ti-r131 linux-headers-4.14.108-ti-r131 ti-sgx-ti335x-modules-4.14.108-ti-r131 libdrm-dev ti-libgbm2-dev build-essential make cmake autoconf automake libtool libpulse-dev libdbus-1-dev libudev-dev libibus-1.0-dev libxkbcommon-dev python3 
     apt-get install -y -t stretch ti-sgx-ti33x-ddk-um ti-sgx-common-ddk-um
     touch /etc/sgx-installed
     reboot
